3bd1571cfc14a6891cc80db16ef27059139f63a2
Georgios Zacharidis <zageo@outlook.com>
2023-01-12 14:10:40 +0200

    HelloPrinter class added

---
+++ Session-04/Session-04/HelloPrinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04
+{
+    internal class HelloPrinter
+    {
+
+        public String Print() {
+
+            string myName = "Georgios";
+            return ("Hello, " + myName);
+
+
+        }
+
+
+    }
+}
