4fa9c22302cd294955260a59880c893198f5b2f4
Georgios Zacharidis <zageo@outlook.com>
2023-01-12 14:10:07 +0200

    Session-04 added

---
+++ Session-04/Session-04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Session_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Session-04 ");
+            Console.WriteLine("Written by Georgios Zacharidis");
+            Console.WriteLine();
+
+
+            Console.WriteLine("Exercise 1");
+            HelloPrinter helloPrinter = new HelloPrinter();
+            Console.WriteLine(helloPrinter.Print());
+
+            Console.WriteLine();
+            Console.WriteLine("Exercise 2");
+
+
+        }
+    }
+}
