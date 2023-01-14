2352b153b33c438c745b8bc61a7f49b6275655de
Georgios Zacharidis <zageo@outlook.com>
2023-01-14 21:29:44 +0200

    Session05 initial push

---
+++ Session-05/Session-05/NameReversal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Session_05
+{
+    internal class NameReversal
+    {
+
+
+        public string Reverse(string name)
+        {
+            char[] reversedName = name.ToCharArray();
+            string reName = "";
+
+            for (int i = reversedName.Length - 1; i >= 0; i--)
+            {
+                reName+= reversedName[i];
+            }
+
+
+
+                return reName;
+        }
+
+    }
+}
---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Session_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Session-05");
+            Console.WriteLine("By Georgios Zacharidis");
+            Console.WriteLine();
+
+
+            Console.WriteLine("Exercise 1");
+            NameReversal nameReversal = new NameReversal();
+            Console.WriteLine("The reverse of my name");
+            string myName = "Georgios";
+            Console.WriteLine($"My name is {myName}");
+            Console.WriteLine("The reverse is " + nameReversal.Reverse(myName));
+
+
+
+            
+
+
+
+        }
+    }
+}
