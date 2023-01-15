4c58644f91977e397fd2e068b2090ea176b61ea1
Georgios Zacharidis <zageo@outlook.com>
2023-01-15 13:10:19 +0200

    Excercise 2 added in Program

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -17,7 +17,44 @@
             Console.WriteLine("The reverse is " + nameReversal.Reverse(myName));
 
 
+            Console.WriteLine();
+            Console.WriteLine("Exercise 2");
 
+            bool exitFlag = false;
+
+
+            do
+            {
+                Console.WriteLine("Please enter a valid option : ");
+                Console.WriteLine("Enter 1 for calculating the sum of numbers 1..n ");
+                Console.WriteLine("Enter 2 for calculating the product of numbers 1..n");
+
+                int inputChoice = Convert.ToInt32(Console.ReadLine());
+
+                if (inputChoice == 1 || inputChoice == 2) {
+
+                    exitFlag = true; // to exit the loop 
+
+                    Console.WriteLine("Now please enter a positive integer : ");
+                    int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+                    if (inputChoice == 1)
+                    {
+                        // call sum calculation
+                    } else
+                    {
+                        // call product calculation
+                    }
+                    
+
+
+                    
+                }
+
+
+
+
+            } while(!exitFlag);
             
 
 
