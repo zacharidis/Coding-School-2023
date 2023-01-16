655689dd16044000a164307ae97925f5292f0326
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 14:56:33 +0200

    added Professor class

---
+++ Session-06/Session-06/Stuff/Professor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06.Stuff
+{
+    internal class Professor
+    {
+    }
+}
