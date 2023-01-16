29040bec3248ef6777dd330acb3c423e0974a48e
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 16:44:40 +0200

    Properties and Constructors added in Institute class

--- Session-06/Session-06/Academics/Institute.cs
+++ Session-06/Session-06/Academics/Institute.cs
@@ -8,5 +8,36 @@ namespace Session_06.Academics
 {
     internal class Institute
     {
+        // properties
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public int YearsInService { get; set; }
+
+
+        // constructor
+
+        public Institute()
+        {
+
+        }
+
+        public Institute(Guid id)
+        {
+            this.ID = id;
+        }
+
+        public Institute(string name)
+        {
+            this.Name = name;
+        }
+
+        public Institute(Guid iD, string name, int yearsInService)
+        {
+            this.YearsInService = yearsInService;
+            this.Name = name;
+            this.YearsInService = yearsInService;
+        }
+
+
     }
 }
