31f67dca4c2e376c0ccdb13e9af3184c1f7acf72
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 15:41:57 +0200

    Schedule class finalised

--- Session-06/Session-06/Helpers/Schedule.cs
+++ Session-06/Session-06/Helpers/Schedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,46 @@ namespace Session_06.Helpers
 {
     internal class Schedule
     {
+
+        public Guid ID { get; set; }
+        public Guid CourseID     { get; set; }
+
+        public Guid ProfessorID { get; set; }
+        public Calendar? DateTime { get; set; }
+
+        public Schedule()
+        {
+        }
+
+        public Schedule(Guid id)
+        {
+            this.ID = id;
+        }
+
+        public Schedule(Guid id, Guid courseId) 
+        {
+            this.ID = id;
+            this.CourseID = courseId;
+        }
+
+        public Schedule(Guid id, Guid courseId , Guid professorId)
+        {
+            this.ID = id;
+            this.CourseID = courseId;   
+            this.ProfessorID = professorId;
+
+        }
+
+        public Schedule(Guid id, Guid courseId, Guid professorId , Calendar calendar)
+        {
+            this.ID = id;
+            this.CourseID = courseId;
+            this.ProfessorID = professorId;
+            this.DateTime= calendar;
+
+        }
+
+
+
     }
 }
