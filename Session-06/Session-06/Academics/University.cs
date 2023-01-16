be5af40cd497ef48726690f6aa3f9ecca3150192
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 17:16:17 +0200

    Minor changes in University class

--- Session-06/Session-06/Academics/University.cs
+++ Session-06/Session-06/Academics/University.cs
@@ -12,11 +12,63 @@ namespace Session_06.Academics
     {
         // properties
         public Student[]? Students { get; set; }
-        public Course[] Courses { get; set; }
+        public Course[]? Courses { get; set; }
 
-        public Grade[] Grades { get; set; }
+        public Grade[]? Grades { get; set; }
 
-        public Schedule[] ScheduledCourse { get; set; }
+        public Schedule[]? ScheduledCourse { get; set; }
+
+
+        // Constructor 
+
+        public University()
+        {
+
+        }
+
+        public University(Guid id) : base(id) { 
+            this.ID = id;
+
+        }
+
+        public University(Guid id ,string name , int yearsInService): base(id,name,yearsInService) {
+            this.ID = id;
+            this.Name = name;
+            this.YearsInService = yearsInService;
+
+        }
+        
+        public University(Guid id, string name , int yearsInService , Student[] students): base(id, name, yearsInService) {}
+
+        public University(Guid id , string name , int yearsInService , Student[] students, Grade[] grades, Course[] courses, Schedule[] scheduledCourse) : base(id,name,yearsInService)
+        {
+
+        }
+
+        // methods 
+
+
+        public Student[] GetStudents()
+        {
+            return this.Students;
+        }
+        
+        public Grade[] GetGrades()
+        {
+            return this.Grades;
+        }
+
+        public Course[] GetCourses()
+        {
+            return this.Courses;
+        }
+        
+
+        public void SetSchedule(Course courseId , Professor professorId , DateTime dateTime)
+        {
+            // business logic goes here
+        }
+
 
     }
 }
