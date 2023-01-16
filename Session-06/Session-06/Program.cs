f1e37f52b842d16a0ab0bb62f019b0ec73b56931
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 14:43:08 +0200

    Initial commit for session 06. Person class added

---
+++ Session-06/Session-06/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Session_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Session-06 - OOP & Inheritance");
+            Console.WriteLine();
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Session-06/Session-06/Stuff/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06.Stuff
+{
+    internal class Person
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+
+        // CONSTRUCTORS
+        // default constructor 
+        public Person()
+        {
+
+        }
+
+        public Person(Guid id)
+        {
+            this.Id = id;
+        }
+
+        public Person(Guid  id, string name )
+        {
+
+        }
+        public Person(Guid id , string name , int age)
+        {   
+                
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+
+
+        // methods 
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public  void SetName(string name) { }
+    }
+}
