e4e74f09d2eb9aa02a43438ad6faf3e1e6d18913
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 16:38:10 +0200

    Student class finalized

--- Session-06/Session-06/Stuff/Student.cs
+++ Session-06/Session-06/Stuff/Student.cs
@@ -9,8 +9,71 @@ namespace Session_06.Stuff
 {
     internal class Student : Person
     {
+        //properties
         public int RegistrationNumber { get; set; }
-        public Course Courses { get; set; } 
+        public Course[]? Courses { get; set; } 
+
+        //constructors
+        public Student() { }
+
+    
+
+        public Student (Guid id) : base(id)
+        {
+            this.Id = id;
+        }
+
+
+        public Student(Guid id, string name) : base(id, name)
+        {
+            this.Id = id;
+            this.Name = name;
+
+        }
+
+        public Student(Guid id, string name, int age) : base(id, name, age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public Student(int registrationNumber, Course[]? courses)
+        {
+            this.RegistrationNumber = registrationNumber;
+            this.Courses = courses;
+        }
+
+        public Student(int registrationNumber)
+        {
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public Student(Guid id, string name, int age , int registrationNumber) : base(id, name, age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public Student(Guid id, string name, int age,int registrationNumber , Course[] courses) : base(id, name, age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.RegistrationNumber = registrationNumber;
+            this.Courses = courses;
+        }
+
+        // methods
+
+        public void Attend(Course course, DateTime datetime)
+        {
+
+        }
+
+        public void WriteExam(Course course , DateTime date) {}
 
 
     }
