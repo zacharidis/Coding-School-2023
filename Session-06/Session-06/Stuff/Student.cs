fe200ec5167a616b7d7f0af3481c61fb51c15b77
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 14:55:15 +0200

    Folder Stuff added. Containing Person , Professor , Student classes

---
+++ Session-06/Session-06/Stuff/Student.cs
@@ -0,0 +1,17 @@
+﻿using Session_06.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06.Stuff
+{
+    internal class Student : Person
+    {
+        public int RegistrationNumber { get; set; }
+        public Course Courses { get; set; } 
+
+
+    }
+}
