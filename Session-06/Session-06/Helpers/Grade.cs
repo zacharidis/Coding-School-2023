cf9d29f2363dff936ab042d23b9544f9384c0522
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 15:06:00 +0200

    Grade class finalised

--- Session-06/Session-06/Helpers/Grade.cs
+++ Session-06/Session-06/Helpers/Grade.cs
@@ -8,5 +8,53 @@ namespace Session_06.Helpers
 {
     internal class Grade
     {
+        // properties
+        public Guid ID { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid CourseID { get; set; }  
+
+        public int GradeValue { get; set; }
+
+        // Constructors
+        public Grade() { }
+
+        public Grade(Guid id)
+        {
+            this.ID = id;   
+        }
+
+        public Grade(Guid id , Guid studentId)
+        {
+
+            this.ID = id;
+            this.StudentId = studentId;
+
+              
+        }
+
+        public Grade(Guid id  , Guid  studentId , Guid courseId )
+        {
+            this.ID= id;
+            this.StudentId = studentId;
+            this.CourseID = courseId;
+        }
+
+
+        public Grade(Guid iD, Guid studentId, Guid courseID, int gradeValue) 
+        {
+            this .ID = iD;
+            this .StudentId = studentId;
+            this.CourseID = courseID;
+            this.GradeValue = gradeValue;
+
+        }
+
+
+        // methods 
+
+
+
+
+
     }
 }
