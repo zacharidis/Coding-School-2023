52ecef8b887d76608752bc2a5b3ea8b1b2e9d834
Georgios Zacharidis <zageo@outlook.com>
2023-01-16 16:50:30 +0200

    Properties added to University Class

--- Session-06/Session-06/Academics/University.cs
+++ Session-06/Session-06/Academics/University.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Session_06.Helpers;
+using Session_06.Stuff;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace Session_06.Academics
 {
-    internal class University
+    internal class University:Institute
     {
+        // properties
+        public Student[]? Students { get; set; }
+        public Course[] Courses { get; set; }
+
+        public Grade[] Grades { get; set; }
+
+        public Schedule[] ScheduledCourse { get; set; }
+
     }
 }
