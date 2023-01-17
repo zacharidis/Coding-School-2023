8fcfc028910983b43e26cd2b73e703b71853d069
Georgios Zacharidis <zageo@outlook.com>
2023-01-17 15:49:26 +0200

    Initial push of Session-07

---
+++ Session-07/Session-07/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Session-07");
+        }
+    }
+}
