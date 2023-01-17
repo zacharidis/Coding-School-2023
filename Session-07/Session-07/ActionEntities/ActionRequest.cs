225de606914abad54914fa947a8a7a0fda0ad594
Georgios Zacharidis <zageo@outlook.com>
2023-01-17 16:25:07 +0200

    ActionRequest class created

--- Session-07/Session-07/ActionEntities/ActionRequest.cs
+++ Session-07/Session-07/ActionEntities/ActionRequest.cs
@@ -8,5 +8,15 @@ namespace Session_07.ActionEntities
 {
     internal class ActionRequest
     {
+        // properties 
+        public Guid RequestID { get; set; }
+        public String Input { get; set; }
+        public Action ActionEnum { get; set; }
+
+
+        public ActionRequest()
+        {
+
+        }
     }
 }
