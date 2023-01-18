cb0a9013a582be257b2d3204b05e2f61ac675692
Georgios Zacharidis <zageo@outlook.com>
2023-01-18 17:06:10 +0200

    ActionRequest extends ActionEntity for inheritance purposes

--- Session-07/Session-07/ActionEntities/ActionResponse.cs
+++ Session-07/Session-07/ActionEntities/ActionResponse.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace Session_07.ActionEntities
 {
-    internal class ActionResponse
+    internal class ActionResponse : ActionEntity
     {
-        public Guid RequestID { get; set; }
+       
         public Guid ResponseID { get; set; }    
         public String Output { get; set; }
 
 
         public ActionResponse()
         {
+            ResponseID = Guid.NewGuid();
 
         }
     }
