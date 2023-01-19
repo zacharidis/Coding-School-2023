88055127b7bc15a049240c347cf28583867472e6
Georgios Zacharidis <zageo@outlook.com>
2023-01-19 13:49:36 +0200

    all numeric keys are working

--- Session-09/Session-09/Form1.Designer.cs
+++ Session-09/Session-09/Form1.Designer.cs
@@ -240,6 +240,7 @@
             this.button12.TabIndex = 11;
             this.button12.Text = "0";
             this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button10
             // 
@@ -264,6 +265,7 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button8
             // 
@@ -276,6 +278,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button7
             // 
@@ -288,6 +291,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button6
             // 
@@ -300,6 +304,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
@@ -312,6 +317,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
@@ -324,6 +330,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
--- Session-09/Session-09/Form1.cs
+++ Session-09/Session-09/Form1.cs
@@ -29,5 +29,43 @@ namespace Session_09
         {
             ctrlTxtExpression.Text = ctrlTxtExpression.Text + "3";
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ctrlTxtExpression.Text= ctrlTxtExpression.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ctrlTxtExpression.Text=ctrlTxtExpression.Text + "5";
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ctrlTxtExpression.Text = ctrlTxtExpression.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ctrlTxtExpression.Text = ctrlTxtExpression.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ctrlTxtExpression.Text = ctrlTxtExpression.Text + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ctrlTxtExpression.Text = ctrlTxtExpression.Text + "9";
+
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            ctrlTxtExpression.Text = ctrlTxtExpression.Text + "0";
+        }
     }
 }
