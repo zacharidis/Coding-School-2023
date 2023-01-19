5eaa648149d68e9a9018cc77a67735cf4b33e308
Georgios Zacharidis <zageo@outlook.com>
2023-01-19 12:57:25 +0200

    Initiala Interface commit

--- Session-09/Session-09/Form1.Designer.cs
+++ Session-09/Session-09/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ctrlGrougBoxResults = new System.Windows.Forms.GroupBox();
+            this.ctrlTxtResults = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ctrlGrougBoxResults.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ctrlGrougBoxResults
+            // 
+            this.ctrlGrougBoxResults.Controls.Add(this.ctrlTxtResults);
+            this.ctrlGrougBoxResults.ForeColor = System.Drawing.Color.White;
+            this.ctrlGrougBoxResults.Location = new System.Drawing.Point(22, 12);
+            this.ctrlGrougBoxResults.Name = "ctrlGrougBoxResults";
+            this.ctrlGrougBoxResults.Size = new System.Drawing.Size(446, 111);
+            this.ctrlGrougBoxResults.TabIndex = 0;
+            this.ctrlGrougBoxResults.TabStop = false;
+            this.ctrlGrougBoxResults.Text = "Results :";
+            // 
+            // ctrlTxtResults
+            // 
+            this.ctrlTxtResults.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ctrlTxtResults.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ctrlTxtResults.ForeColor = System.Drawing.Color.White;
+            this.ctrlTxtResults.Location = new System.Drawing.Point(16, 22);
+            this.ctrlTxtResults.Name = "ctrlTxtResults";
+            this.ctrlTxtResults.Size = new System.Drawing.Size(415, 71);
+            this.ctrlTxtResults.TabIndex = 0;
+            this.ctrlTxtResults.Text = "RESULTS";
+            this.ctrlTxtResults.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(22, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(446, 280);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(492, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ctrlGrougBoxResults);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ctrlGrougBoxResults.ResumeLayout(false);
+            this.ctrlGrougBoxResults.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GroupBox ctrlGrougBoxResults;
+        private TextBox ctrlTxtResults;
+        private GroupBox groupBox1;
     }
 }
