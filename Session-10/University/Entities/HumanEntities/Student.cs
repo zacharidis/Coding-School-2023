dc231b46c8ef6dca4d5891488a6719b9bff7786a
Georgios Zacharidis <zageo@outlook.com>
2023-01-20 14:43:18 +0200

    Student class minimalised

--- Session-10/University/Entities/HumanEntities/Student.cs
+++ Session-10/University/Entities/HumanEntities/Student.cs
@@ -18,62 +18,7 @@ namespace Session_06.Stuff
 
     
 
-        public Student (Guid id) : base(id)
-        {
-            this.Id = id;
-        }
-
-
-        public Student(Guid id, string name) : base(id, name)
-        {
-            this.Id = id;
-            this.Name = name;
-
-        }
-
-        public Student(Guid id, string name, int age) : base(id, name, age)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.Age = age;
-        }
-
-        public Student(int registrationNumber, Course[]? courses)
-        {
-            this.RegistrationNumber = registrationNumber;
-            this.Courses = courses;
-        }
-
-        public Student(int registrationNumber)
-        {
-            this.RegistrationNumber = registrationNumber;
-        }
-
-        public Student(Guid id, string name, int age , int registrationNumber) : base(id, name, age)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.Age = age;
-            this.RegistrationNumber = registrationNumber;
-        }
-
-        public Student(Guid id, string name, int age,int registrationNumber , Course[] courses) : base(id, name, age)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.Age = age;
-            this.RegistrationNumber = registrationNumber;
-            this.Courses = courses;
-        }
-
-        // methods
-
-        public void Attend(Course course, DateTime datetime)
-        {
-
-        }
-
-        public void WriteExam(Course course , DateTime datetime) {}
+    
 
 
     }
