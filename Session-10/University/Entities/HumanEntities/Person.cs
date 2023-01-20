3a71ccbcb80d8d5dd58cbe20430941d1da93a426
Georgios Zacharidis <zageo@outlook.com>
2023-01-20 14:37:28 +0200

    Person class improved , adding gender and graduation fields.

--- Session-10/University/Entities/HumanEntities/Person.cs
+++ Session-10/University/Entities/HumanEntities/Person.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace Session_06.Stuff
 {
+
+    public enum GenderEnum {Male = 0 , Female = 1 , Other = 2} 
     internal class Person
     {
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public int Age { get; set; }
+
+        public bool Undergraduate { get; set; }
+
+        public GenderEnum Gender { get; set; } 
+
 
 
         // CONSTRUCTORS
