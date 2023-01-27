f6199a3bc3c8718cd62a09675219f582a20a790c
Georgios Zacharidis <zageo@outlook.com>
2023-01-27 13:59:10 +0200

    Set image to null according to checkbox value

--- Session-15/Session-15/Form1.cs
+++ Session-15/Session-15/Form1.cs
@@ -9,36 +9,68 @@ namespace Session_15
 
         private void chkStew_CheckedChanged(object sender, EventArgs e)
         {
-            pbStew.Image = Resource1.stew;
+            
+           
         }
 
         private void chkFries_CheckedChanged(object sender, EventArgs e)
         {
-            pbFries.Image = Resource1.fries;
+            
         }
 
         private void chkSalad_CheckedChanged(object sender, EventArgs e)
         {
-            pbSalad.Image = Resource1.Salad;
+            
         }
 
         private void chkPepsi_CheckedChanged(object sender, EventArgs e)
         {
-            pbPepsi.Image = Resource1.pepsi;
+           
         }
 
         private void btnPrepare_Click(object sender, EventArgs e)
         {
             int progress = (int) groupBox2.Width;
             int movements = (int) (progress / pbStew.Width);
-            
 
-            pbStew.Left += movements;
+            FillImages();
+
+
+
         }
 
         private void btnAbout_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Simple Kitchen emulator that uses ASYNC / AWAIT ", "By Georgios Zacharidis", MessageBoxButtons.OK);
+        }
+
+
+        private void FillImages()
+        {
+            if (chkFries.Checked)
+            {
+                pbFries.Image = Resource1.fries;
+            } else { pbFries.Image = null; } 
+
+            if (chkPepsi.Checked)
+            {
+                pbPepsi.Image = Resource1.pepsi;
+            }
+
+            if (chkSalad.Checked)
+            {
+                pbSalad.Image = Resource1.Salad;
+
+            }
+
+            if (chkStew.Checked)
+            {
+                pbStew.Image = Resource1.stew;
+
+            }
+
+
+
         }
     }
 }
