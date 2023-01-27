09d34470ffb476fefd0f35acab9d20529ec0ba08
Georgios Zacharidis <zageo@outlook.com>
2023-01-27 14:40:47 +0200

    Fixed Empty menu Order

--- Session-15/Session-15/Form1.cs
+++ Session-15/Session-15/Form1.cs
@@ -47,28 +47,37 @@ namespace Session_15
 
         private void FillImages()
         {
+
+            int counter = 0;
             if (chkFries.Checked)
             {
                 pbFries.Image = Resource1.fries;
+                counter++;
             } else { pbFries.Image = null; } 
 
             if (chkPepsi.Checked)
             {
                 pbPepsi.Image = Resource1.pepsi;
-            }
+                counter++;
+            } else { pbPepsi.Image = null; }
 
             if (chkSalad.Checked)
             {
                 pbSalad.Image = Resource1.Salad;
+                counter++;
 
-            }
+            } else { pbSalad.Image = null; }
 
             if (chkStew.Checked)
             {
                 pbStew.Image = Resource1.stew;
+                counter++;
 
+            } else { pbStew.Image = null; }
+
+            if(counter == 0 ) {
+                MessageBox.Show("Nothing to eat !", "Empty Order", MessageBoxButtons.OK);
             }
-
 
 
         }
