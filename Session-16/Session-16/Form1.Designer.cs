61659f2ce8132209e366dcb58a83a0e16996bae0
Georgios Zacharidis <zageo@outlook.com>
2023-01-30 16:40:05 +0200

    Project within solution renamed properly

--- Session-16/Session-16/Form1.Designer.cs
+++ Session-16/Session-16/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1063, 789);
+            ClientSize = new Size(1000, 754);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
