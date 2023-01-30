382e285cc14b292c4f132f75a2178791cd8e97a4
Georgios Zacharidis <zageo@outlook.com>
2023-01-30 22:54:52 +0200

    Added all properties to DbContext class. One for each entity

--- Session-16/EF.CoffeeShop.Orm/CoffeeShopDbContext.cs
+++ Session-16/EF.CoffeeShop.Orm/CoffeeShopDbContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Libs;
+using Microsoft.EntityFrameworkCore;
+using Session_11;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,22 @@ namespace EF.CoffeeShop.Orm
 {
     internal class CoffeeShopDbContext
     {
+        public DbSet<Product> products { get; set; }
+
+        public DbSet<ProductCategory> productCategories { get; set; }
+        public DbSet<Transaction> transactions { get; set; }
+
+        public DbSet<TransactionLine> transactionLines { get; set; }
+
+        public DbSet<MonthlyLedger> monthlyLedgers { get; set; }
+
+        public DbSet<Employee> employees { get; set; }
+
+        public DbSet<Customer> customers { get; set; }
+
+
+
+     
+
     }
 }
