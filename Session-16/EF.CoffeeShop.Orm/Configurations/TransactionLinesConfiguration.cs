7d31bb7fc82027393d18b8f6e6419b42070982f1
Georgios Zacharidis <zageo@outlook.com>
2023-01-31 14:59:48 +0200

    Finalized TransactionLinesConfiguration

--- Session-16/EF.CoffeeShop.Orm/Configurations/TransactionLinesConfiguration.cs
+++ Session-16/EF.CoffeeShop.Orm/Configurations/TransactionLinesConfiguration.cs
@@ -13,7 +13,30 @@ namespace EF.CoffeeShop.Orm.Configurations
     {
         public void Configure(EntityTypeBuilder<TransactionLine> builder)
         {
-            throw new NotImplementedException();
+
+            builder.ToTable("TRANSACTION LINE");
+            
+            //primary key 
+            builder.HasKey(transactionLine => transactionLine.ID);
+
+            //props 
+            builder.Property(transactionLine => transactionLine.ID);
+
+            builder.Property(transactionLine => transactionLine.Quantity)
+                .HasColumnType("decimal(6,2)").HasPrecision(5, 2);
+
+            builder.Property(transactionLine => transactionLine.Discount)
+                .HasColumnType("decimal(6,2)").HasPrecision(5, 2);
+            
+            builder.Property(transactionLine => transactionLine.Description)
+                .HasMaxLength(250);
+            
+            builder.Property(transactionLine => transactionLine.Price)
+                .HasColumnType("decimal(5,2)").HasPrecision(5, 2);
+
+            //relationships 
+
+
         }
     }
 }
