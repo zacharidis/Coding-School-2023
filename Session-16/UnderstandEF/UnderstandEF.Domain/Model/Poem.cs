8138b82dfbf49cd00b29c3d815445de72c4caff1
Georgios Zacharidis <zageo@outlook.com>
2023-02-01 15:25:41 +0200

    finished Poem class

--- Session-16/UnderstandEF/UnderstandEF.Domain/Model/Poem.cs
+++ Session-16/UnderstandEF/UnderstandEF.Domain/Model/Poem.cs
@@ -8,5 +8,16 @@ namespace UnderstandEF.Domain.Model
 {
     internal class Poem
     {
+        public int Id { get; set; }
+        public string PoemBody { get; set; }
+        public string PoemName { get; set;  }
+        
+        // EF ref
+        public Poet Poet { get; set; }
+
+        public int PoetID { get; set; }
+
+        // EF ref
+
     }
 }
