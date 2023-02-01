59fdd08e856b74e859f20e87521d1e3280415e30
Georgios Zacharidis <zageo@outlook.com>
2023-02-01 15:22:31 +0200

    Added blueprint for my two classes , Poet and Poem

---
+++ Session-16/UnderstandEF/UnderstandEF.Domain/Model/Poet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandEF.Domain.Model
+{
+    internal class Poet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+
+
+    }
+}
