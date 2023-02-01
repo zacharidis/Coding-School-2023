0d5c7532367132a63977efebc6c15a0c35cbb199
Georgios Zacharidis <zageo@outlook.com>
2023-02-01 15:58:14 +0200

    added configuration classes

---
+++ Session-16/UnderstandEF/UnderstandEF.Data/Configurations/PoemConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandEF.Orm.Configurations
+{
+    internal class PoemConfiguration
+    {
+    }
+}
