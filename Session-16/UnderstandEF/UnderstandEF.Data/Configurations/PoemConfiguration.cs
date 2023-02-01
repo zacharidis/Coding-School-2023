0c164f568c9d4df6d31b90e9be83579475af68bc
Georgios Zacharidis <zageo@outlook.com>
2023-02-01 15:58:41 +0200

    Configuration classes implement the IentityConfiguration interface

--- Session-16/UnderstandEF/UnderstandEF.Data/Configurations/PoemConfiguration.cs
+++ Session-16/UnderstandEF/UnderstandEF.Data/Configurations/PoemConfiguration.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnderstandEF.Domain.Model;
 
 namespace UnderstandEF.Orm.Configurations
 {
-    internal class PoemConfiguration
+    public class PoemConfiguration : IEntityTypeConfiguration<Poem>
     {
+        public void Configure(EntityTypeBuilder<Poem> builder)
+        {
+            
+        }
     }
 }
--- Session-16/UnderstandEF/UnderstandEF.Data/Configurations/PoetConfiguration.cs
+++ Session-16/UnderstandEF/UnderstandEF.Data/Configurations/PoetConfiguration.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnderstandEF.Domain.Model;
 
 namespace UnderstandEF.Orm.Configurations
 {
-    internal class PoetConfiguration
+    public class PoetConfiguration : IEntityTypeConfiguration<Poet>
     {
+        public void Configure(EntityTypeBuilder<Poet> builder)
+        {
+           
+        }
     }
 }
