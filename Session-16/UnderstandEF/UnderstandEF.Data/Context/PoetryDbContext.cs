2b46ba743abdea17356512e1c8f4fb128c9c03fc
Georgios Zacharidis <zageo@outlook.com>
2023-02-01 15:38:07 +0200

    Added DbSet for boy my models

--- Session-16/UnderstandEF/UnderstandEF.Data/Context/PoetryDbContext.cs
+++ Session-16/UnderstandEF/UnderstandEF.Data/Context/PoetryDbContext.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnderstandEF.Domain.Model;
 
 namespace UnderstandEF.Orm.Context
 {
     public  class PoetryDbContext : DbContext
     {
-        DbSet<Poet> poets { }
+        DbSet<Poet> Poets { get; set; }
+        DbSet<Poem> Poems { get; set; }
+
+
 
     }
 }
