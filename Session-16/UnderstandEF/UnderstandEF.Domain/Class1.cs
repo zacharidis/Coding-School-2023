3fee9e8372abc00c2ab32e54f5aeaa0720778dd3
Georgios Zacharidis <zageo@outlook.com>
2023-02-01 15:14:31 +0200

    Added Class Library for Domain(includes the objects)

---
+++ Session-16/UnderstandEF/UnderstandEF.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace UnderstandEF.Domain
+{
+    public class Class1
+    {
+
+    }
+}
--- Session-16/UnderstandEF/UnderstandEF/Program.cs
+++ Session-16/UnderstandEF/UnderstandEF/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Simple App that implements EF Core ");
+            Console.WriteLine("Written by Georgios Zacharidis while in Epsilon Coding School 2023");
+            Console.WriteLine("Thank you for visiting");
+            Console.WriteLine("Please check my github profile @: github.com/zacharidis");
+
         }
     }
 }
