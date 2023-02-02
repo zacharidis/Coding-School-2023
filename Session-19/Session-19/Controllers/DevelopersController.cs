ca419a2840cec5d2ab64e5a6f9fe982a2447f7fa
Georgios Zacharidis <zageo@outlook.com>
2023-02-02 15:25:51 +0200

    Simple Controller created that initializes 2 developers and returns them

--- Session-19/Session-19/Controllers/DevelopersController.cs
+++ Session-19/Session-19/Controllers/DevelopersController.cs
@@ -4,16 +4,40 @@ using Session_19.Models;
 namespace Session_19.Controllers
 {
     [ApiController]
-    [Route("Developers")]
+    [Route("[controller]")]
     public class DevelopersController : Controller
     {
-      
-      
-      //[HttpGet]
-      //public IActionResult GetAllDevelopers()
-      //  {
-      //      var developers = new List<Developer>;
 
-      //  }
+
+        [HttpGet]
+        public IActionResult GetAllDevelopers()
+        {
+            var developers = new List<Developer>()
+
+            {
+                new Developer
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "George",
+                    Surname = "Zachs",
+                    YearsOfExperience = 1,
+                    projects = new List<Project>()
+
+                },
+
+                new Developer
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Zedd",
+                    Surname = "Geo",
+                    YearsOfExperience = 1,
+                    projects = new List<Project>()
+
+                }
+
+
+            };
+            return Ok(developers);
+        }
     }
 }
