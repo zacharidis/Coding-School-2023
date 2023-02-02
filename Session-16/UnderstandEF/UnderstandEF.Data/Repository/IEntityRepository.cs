369bdd168023180b1a862680ea8e8d157a5062df
Georgios Zacharidis <zageo@outlook.com>
2023-02-02 09:57:30 +0200

    Created the inteface

---
+++ Session-16/UnderstandEF/UnderstandEF.Data/Repository/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandEF.Orm.Repository
+{
+    public interface IEntityRepository<TEntity>
+        where TEntity : // missing the class
+    {
+
+    }
+}
