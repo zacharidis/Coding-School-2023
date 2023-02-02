509e389e9907304b1fa2fdb585278cf0dce14128
Georgios Zacharidis <zageo@outlook.com>
2023-02-02 14:08:00 +0200

    Added Controller for Developers

---
+++ Session-19/Session-19/Controllers/DevelopersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Session_19.Models;
+
+namespace Session_19.Controllers
+{
+    [ApiController]
+    [Route("Developers")]
+    public class DevelopersController : Controller
+    {
+      
+      
+      //[HttpGet]
+      //public IActionResult GetAllDevelopers()
+      //  {
+      //      var developers = new List<Developer>;
+
+      //  }
+    }
+}
