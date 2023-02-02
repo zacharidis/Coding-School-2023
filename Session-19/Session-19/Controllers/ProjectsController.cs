18b37221ae0d5b2296f0e6dd20003ad8bfeadc54
Georgios Zacharidis <zageo@outlook.com>
2023-02-02 15:43:49 +0200

    Added Controller for Projects. Successfully returns dummy data as an OK response

---
+++ Session-19/Session-19/Controllers/ProjectsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Session_19.Models;
+
+namespace Session_19.Controllers
+{
+    [ApiController]
+    [Route("projects")]
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllProjects()
+        {
+            var projects = new List<Project>()
+            {
+                new Project
+                {
+                    Id = Guid.NewGuid(),
+                    ProjectName = "AMAZING PROJECT",
+                    ProjectDescription = "SOMETHING AMAZING",
+                    StartDate = DateTime.Now,
+                    ProjectVersion = "1.0.0",
+                    developers = new List<Developer>()
+
+
+                },
+
+                new Project
+                {
+                    Id = Guid.NewGuid(),
+                    ProjectName = "SECRET PROJECT",
+                    ProjectDescription = "SOMETHING EXTRORDINARY",
+                    StartDate = DateTime.Now,
+                    ProjectVersion = "3.0.0",
+                    developers = new List<Developer>()
+
+
+                }
+            };
+
+            return Ok(projects);
+        }
+    }
+}
