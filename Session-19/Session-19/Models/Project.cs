0fe05c01f74e84512fe62cc9440c2ab4567d697a
Georgios Zacharidis <zageo@outlook.com>
2023-02-02 13:36:15 +0200

    Properties for Project added , not final.

--- Session-19/Session-19/Models/Project.cs
+++ Session-19/Session-19/Models/Project.cs
@@ -2,5 +2,21 @@
 {
     public class Project
     {
+        public Guid Id { get; set; }
+        public string ProjectName { get; set; }
+
+        public string ProjectVersion { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string ProjectDescription { get; set; } 
+
+        public List<Developer> developers { get; set; }
+
+
+        public Project() { }
+      
     }
 }
