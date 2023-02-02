efb0e61cbea0c1f3eb5b156b3f760555a9132d19
Georgios Zacharidis <zageo@outlook.com>
2023-02-02 13:32:14 +0200

    Created properties and def constructor for Developer Class

--- Session-19/Session-19/Models/Developer.cs
+++ Session-19/Session-19/Models/Developer.cs
@@ -2,5 +2,17 @@
 {
     public class Developer
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public int YearsOfExperience { get; set; }
+
+        public List<Project> projects { get; set; }
+
+
+        // def ctor 
+        public Developer() { }
+
     }
 }
