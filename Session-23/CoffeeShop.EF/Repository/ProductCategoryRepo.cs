3038c0bc96ae4a60e9801bf35ad340c057a8dd24
Georgios Zacharidis <zageo@outlook.com>
2023-02-10 16:05:12 +0200

    Added ProductCategory Repo

---
+++ Session-23/CoffeeShop.EF/Repository/ProductCategoryRepo.cs
@@ -0,0 +1,39 @@
+﻿using CoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Repository
+{
+    internal class ProductCategoryRepo : IEntityRepo<ProductCategory>
+
+
+    {
+        public void Add(ProductCategory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id, ProductCategory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductCategory> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, ProductCategory entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
