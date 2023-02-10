45c6fcb33f50ae33fd4cd8c533a979448ba177e0
Georgios Zacharidis <zageo@outlook.com>
2023-02-10 15:39:19 +0200

    Added productRepo

---
+++ Session-23/CoffeeShop.EF/Repository/ProductRepo.cs
@@ -0,0 +1,38 @@
+﻿using CoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Repository
+{
+    internal class ProductRepo : IEntityRepo<Product>
+
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id, Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
