99da63d6bba05dc5af48b3ad9237eaf8a1eaa34b
Georgios Zacharidis <zageo@outlook.com>
2023-02-10 08:03:08 +0200

    Added IEntityRepo

---
+++ Session-23/CoffeeShop.EF/Repository/IEntityRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Repository
+{
+    internal interface IEntityRepo<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity? GetById(int id);
+        void Add(TEntity entity);
+        void Update(int id, TEntity entity);
+        void Delete(int id ,TEntity entity);
+
+    }
+}
