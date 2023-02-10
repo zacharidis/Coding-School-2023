7c0056661bc12261532f5b9f7648802990a3e472
Georgios Zacharidis <zageo@outlook.com>
2023-02-10 15:30:25 +0200

    Added interface methods

--- Session-23/CoffeeShop.EF/Repository/CustomerRepo.cs
+++ Session-23/CoffeeShop.EF/Repository/CustomerRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CoffeeShop.EF.Context;
+using CoffeeShop.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace CoffeeShop.EF.Repository
 {
-    internal class CustomerRepo
+    internal class CustomerRepo : IEntityRepo<Customer>
     {
+        public void Add(Customer entity)
+        {
+            using var context = new CoffeeShopDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id, Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Customer entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
