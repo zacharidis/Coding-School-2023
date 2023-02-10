32104047b3b684aee88417629c04444cb46cd651
Georgios Zacharidis <zageo@outlook.com>
2023-02-10 08:36:18 +0200

    database updated

--- Session-23/CoffeeShop.EF/Context/CoffeeShopDbContext.cs
+++ Session-23/CoffeeShop.EF/Context/CoffeeShopDbContext.cs
@@ -27,7 +27,7 @@ namespace CoffeeShop.EF.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CoffeeShop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("Data Source=COREDEV\\SQLEXPRESS;Initial Catalog=CoffeeShop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             base.OnConfiguring(optionsBuilder);
         }
     }
