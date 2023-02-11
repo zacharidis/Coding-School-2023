7b7ebae3565e4ca8b5ad4078306692872050b22c
Georgios Zacharidis <zageo@outlook.com>
2023-02-11 20:57:40 +0200

    Added transaction repo

---
+++ Session-23/CoffeeShop.EF/Repository/TransactionRepo.cs
@@ -0,0 +1,41 @@
+﻿using CoffeeShop.EF.Context;
+using CoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Repository
+{
+    internal class TransactionRepo : IEntityRepo<Transaction>
+    {
+        public void Add(Transaction entity)
+        {
+            using var context = new CoffeeShopDbContext();
+            context.Transactions.Add(entity);
+            context.SaveChanges();
+
+        }
+
+        public void Delete(int id, Transaction entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Transaction> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaction? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Transaction entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
