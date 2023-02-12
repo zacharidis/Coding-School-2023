c2a4e410571dc60aaa217eb7ccaad9d967c562b6
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 14:37:38 +0200

    CustomerCreateDTO

--- Session-23/CoffeeShop.Web/Models/CustomerDTO/CustomerCreateDTO.cs
+++ Session-23/CoffeeShop.Web/Models/CustomerDTO/CustomerCreateDTO.cs
@@ -2,5 +2,8 @@
 {
     public class CustomerCreateDTO
     {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
     }
 }
