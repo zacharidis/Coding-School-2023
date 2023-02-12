5d9b37d499421d3de9bcf68cd0d471f17f299c4e
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:17:13 +0200

    finalized ProductCategoryDTO

--- Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryDTO.cs
@@ -2,5 +2,9 @@
 {
     public class ProductCategoryDTO
     {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+
+        public string? Description { get; set; }
     }
 }
