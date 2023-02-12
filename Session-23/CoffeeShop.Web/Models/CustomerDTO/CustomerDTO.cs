880d2bba29e621fd1ee2b7845f0b38413b214b1e
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 14:34:48 +0200

    Finalized CustomerDTO the main customerDTO

--- Session-23/CoffeeShop.Web/Models/CustomerDTO/CustomerDTO.cs
+++ Session-23/CoffeeShop.Web/Models/CustomerDTO/CustomerDTO.cs
@@ -2,5 +2,10 @@
 {
     public class CustomerDTO
     {
+        public int Id { get; set; }
+
+        public string? Code { get; set; }
+
+        public string? Description { get; set; }
     }
 }
