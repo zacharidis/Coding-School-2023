c216e95303ed09222436bc3515c471722a43cfb1
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:28:19 +0200

    Details finisehd

--- Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryDetailDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryDetailDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace CoffeeShop.Web.Models.ProductCategoryDTO
+﻿using CoffeeShop.Model.Enums;
+
+namespace CoffeeShop.Web.Models.ProductCategoryDTO
 {
     public class ProductCategoryDetailDTO
     {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+        public ProductType ProductType { get; set; }
     }
 }
