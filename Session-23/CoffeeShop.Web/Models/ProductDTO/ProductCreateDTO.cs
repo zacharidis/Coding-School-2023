c689016fe33d55a7a5e95809feb1ad107bfe617b
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:06:54 +0200

    WIP : ProductCategories problem, to be reevaluated

--- Session-23/CoffeeShop.Web/Models/ProductDTO/ProductCreateDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductDTO/ProductCreateDTO.cs
@@ -1,6 +1,17 @@
-﻿namespace CoffeeShop.Web.Models.ProductDTO
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CoffeeShop.Web.Models.ProductDTO
 {
     public class ProductCreateDTO
     {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+
+        public int ProductCategoryId { get; set; }
+        public List<SelectListItem> ProductCategories { get; set; } 
     }
 }
+
