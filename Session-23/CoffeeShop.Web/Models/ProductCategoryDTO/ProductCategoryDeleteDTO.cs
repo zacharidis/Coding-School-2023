a82770e024442822c781a4b1886f3e0c6d4f831e
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:27:01 +0200

    ProductCategoryDeleteDTO

--- Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryDeleteDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryDeleteDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace CoffeeShop.Web.Models.ProductCategoryDTO
+﻿using System.Collections.Specialized;
+
+namespace CoffeeShop.Web.Models.ProductCategoryDTO
 {
     public class ProductCategoryDeleteDTO
     {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+
     }
 }
