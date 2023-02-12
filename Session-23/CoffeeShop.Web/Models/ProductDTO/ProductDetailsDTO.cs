c8258a86bb8e91a746866457aee3767cb6ad98d6
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:01:40 +0200

    ProductEditDTO finalized

--- Session-23/CoffeeShop.Web/Models/ProductDTO/ProductDetailsDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductDTO/ProductDetailsDTO.cs
@@ -1,6 +1,20 @@
-﻿namespace CoffeeShop.Web.Models.ProductDTO
+﻿using CoffeeShop.Model;
+
+namespace CoffeeShop.Web.Models.ProductDTO
 {
     public class ProductDetailsDTO
     {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+
+        public int ProductCategoryId { get; set; }
+
+        public ProductCategory? productCategory { get; set; }
+
+        // something is wrong here !
+
     }
 }
