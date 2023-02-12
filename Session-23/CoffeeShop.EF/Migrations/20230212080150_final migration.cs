6f2299d5f4e366cd3f79ebf17ecf8ba583e6ba93
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 10:05:06 +0200

    final migration

---
+++ Session-23/CoffeeShop.EF/Migrations/20230212080150_final migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeShop.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class finalmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
