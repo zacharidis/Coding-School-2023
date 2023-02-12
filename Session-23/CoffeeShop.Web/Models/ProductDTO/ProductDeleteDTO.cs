f6cec3edadaa285830d5747f130251fb40708e8c
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:03:24 +0200

    ProductDeleteDTO

--- Session-23/CoffeeShop.Web/Models/ProductDTO/ProductDeleteDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductDTO/ProductDeleteDTO.cs
@@ -2,5 +2,12 @@
 {
     public class ProductDeleteDTO
     {
+
+        public int Id { get; set; }
+        public string? Code { get; set; }   
+        public string? Description { get; set; }
+
+        // do i need more details for the delete action ?
+
     }
 }
