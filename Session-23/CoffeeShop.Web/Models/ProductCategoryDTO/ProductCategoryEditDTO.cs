2f4221f08813b7927610c0c5fd03e395580a4732
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:19:47 +0200

    finalized

--- Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryEditDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryEditDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace CoffeeShop.Web.Models.ProductCategoryDTO
+﻿using CoffeeShop.Model.Enums;
+
+namespace CoffeeShop.Web.Models.ProductCategoryDTO
 {
     public class ProductCategoryEditDTO
     {
+        public int Id { get; set; }
+        public string? Code { get; set; } 
+        public string? Name { get; set; }
+
+        public ProductType ProductType { get; set; }
     }
 }
