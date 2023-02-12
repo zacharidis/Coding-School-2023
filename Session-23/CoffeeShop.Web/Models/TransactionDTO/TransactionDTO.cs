91481892d7a73b267a86ec35ecadbb0c100656b6
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:32:00 +0200

    DTOS for transaction added , no delete DTO because a transaction shall not be deleted

---
+++ Session-23/CoffeeShop.Web/Models/TransactionDTO/TransactionDTO.cs
@@ -0,0 +1,12 @@
+﻿using CoffeeShop.Model.Enums;
+
+namespace CoffeeShop.Web.Models.TransactionDTO
+{
+    public class TransactionDTO
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalPrice { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
---
+++ Session-23/CoffeeShop.Web/Models/TransactionDTO/TransactionDetailDTO.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShop.Model;
+using CoffeeShop.Model.Enums;
+
+namespace CoffeeShop.Web.Models.TransactionDTO
+{
+    public class TransactionDetailDTO
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalPrice { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
+
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; } = null!;
+
+        public List<TransactionLine> TransactionLines { get; set; }
+    }
+}
