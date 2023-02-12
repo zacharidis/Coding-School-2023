36b41a35646aedcad7599473decf0b028b7b2adf
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 14:53:43 +0200

    productDTO properties finalized

--- Session-23/CoffeeShop.Web/Models/ProductDTO/ProductDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductDTO/ProductDTO.cs
@@ -2,5 +2,10 @@
 {
     public class ProductDTO
     {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+
+
     }
 }
