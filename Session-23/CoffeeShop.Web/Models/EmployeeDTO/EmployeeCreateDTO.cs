a167135cb4ef083b46823d7740321f003d3318e0
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 14:44:36 +0200

    Added EmployeeCreateDTO

--- Session-23/CoffeeShop.Web/Models/EmployeeDTO/EmployeeCreateDTO.cs
+++ Session-23/CoffeeShop.Web/Models/EmployeeDTO/EmployeeCreateDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace CoffeeShop.Web.Models.EmployeeDTO
+﻿using CoffeeShop.Model.Enums;
+
+namespace CoffeeShop.Web.Models.EmployeeDTO
 {
     public class EmployeeCreateDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int SalaryPerMonth { get; set; }
+        public EmployeeType EmployeeType { get; set; }
     }
 }
