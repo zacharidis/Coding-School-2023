21c88a8bdce25367df304c3cb784bc43d2825f32
Georgios Zacharidis <zageo@outlook.com>
2023-02-12 15:28:11 +0200

    ProductCategoryCreateDTO finished

--- Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryCreateDTO.cs
+++ Session-23/CoffeeShop.Web/Models/ProductCategoryDTO/ProductCategoryCreateDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace CoffeeShop.Web.Models.ProductCategoryDTO
+﻿using CoffeeShop.Model.Enums;
+
+namespace CoffeeShop.Web.Models.ProductCategoryDTO
 {
     public class ProductCategoryCreateDTO
     {
+        public int Id { get; set; }
+        public string? Code { get; set; } 
+        public string? Description { get; set; } 
+        public ProductType ProductType { get; set; }
     }
 }
