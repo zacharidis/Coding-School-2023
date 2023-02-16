ddee9852f019e85e209059063ec44efb9878206c
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 23:04:35 +0200

    Added Patient Configuration class

---
+++ Session-27/EpsilonRevision/EpsilonRevision.EF/Configurations/PatientConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using EpsilonRevision.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonRevision.EF.Configurations
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+       
+
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
