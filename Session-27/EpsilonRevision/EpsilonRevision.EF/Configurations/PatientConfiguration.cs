7b5769c8de2dc5f78df91e6c6b5e4fc25fb057d9
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 23:10:19 +0200

    Patient configuration properties updated

--- Session-27/EpsilonRevision/EpsilonRevision.EF/Configurations/PatientConfiguration.cs
+++ Session-27/EpsilonRevision/EpsilonRevision.EF/Configurations/PatientConfiguration.cs
@@ -16,6 +16,32 @@ namespace EpsilonRevision.EF.Configurations
         public void Configure(EntityTypeBuilder<Patient> builder)
         {
            
+            //table 
+            builder.ToTable(nameof(Patient));
+
+            //primary key
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            //properties
+            builder.Property(p => p.Surname).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Address).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.PhoneNumber).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Email).HasMaxLength(50);
+            builder.Property(p => p.Age).IsRequired();
+            builder.Property(p => p.AMKA).HasMaxLength(50);
+
+      
+
+
+
+
+
+
+
+
+
         }
     }
 }
