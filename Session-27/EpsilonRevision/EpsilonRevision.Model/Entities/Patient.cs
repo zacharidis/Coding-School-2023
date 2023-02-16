9aed780540eea5fb30fca221a40e8635a843a9cc
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 23:06:58 +0200

    Added Age proprty for patient

--- Session-27/EpsilonRevision/EpsilonRevision.Model/Entities/Patient.cs
+++ Session-27/EpsilonRevision/EpsilonRevision.Model/Entities/Patient.cs
@@ -14,6 +14,8 @@ namespace EpsilonRevision.Model.Entities
         public int Id { get; set; }// patients primary key
         public string Name { get; set; }
         public string Surname { get; set; }
+
+        public int Age { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
