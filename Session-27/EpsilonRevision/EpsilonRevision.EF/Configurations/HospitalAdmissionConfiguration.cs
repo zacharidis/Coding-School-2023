358984b69e8a8823cc921458fcc725e86da41598
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 22:28:43 +0200

    Added Configuration class for HospitalAdmission

---
+++ Session-27/EpsilonRevision/EpsilonRevision.EF/Configurations/HospitalAdmissionConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using EpsilonRevision.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonRevision.EF.Configurations
+{
+    public class HospitalAdmissionConfiguration : IEntityTypeConfiguration<HospitalAdmission>
+    {
+        public void Configure(EntityTypeBuilder<HospitalAdmission> builder)
+        {
+            
+            //table
+            
+            builder.ToTable(nameof(HospitalAdmission));
+
+            //primary key
+
+            builder.HasKey(ha => ha.Id);
+            builder.Property(ha=>ha.Id).ValueGeneratedOnAdd();
+
+            //properties 
+            
+
+
+        }
+    }
+}
