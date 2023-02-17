f16a2bff83ad7311f264d71313be9798a3f518b6
Georgios Zacharidis <zageo@outlook.com>
2023-02-17 19:05:18 +0200

    Added properties in Transaction Class

--- Session-30/GZFuel.Model/Entities/Transaction.cs
+++ Session-30/GZFuel.Model/Entities/Transaction.cs
@@ -8,5 +8,36 @@ namespace GZFuel.Model.Entities
 {
     public class Transaction
     {
+        // properties
+        // ID, TransactionID, ItemID, Quantity, 
+       //  ItemPrice, NetValue, DiscountPercent, DiscountValue, 
+       //  TotalValue 
+
+        public int ID { get; set; }
+        public int TransactionID { get; set; }
+        public int ItemID { get; set; }
+        public int Quantity { get; set; }
+        public decimal ItemPrice { get; set; }
+        public decimal NetValue { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal DiscountValue { get; set; }
+        public decimal TotalValue { get; set; }
+
+        // constructor
+        public Transaction()
+        {
+        
+            // Net Value , Discount Percent, Discount Value, Total Value will be calculated in the constructor ! 
+            // Net Value = Quantity * Item Price
+            // Discount Value = Net Value * Discount Percent
+            // Total Value = Net Value - Discount Value
+
+
+        
+        }
+
+
+   
+
     }
 }
