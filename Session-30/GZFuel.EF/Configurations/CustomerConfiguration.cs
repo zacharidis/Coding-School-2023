ee37754740f5b68660ea689f1c911f068efca675
Georgios Zacharidis <zageo@outlook.com>
2023-02-18 12:06:58 +0200

    Minor fix in CustomerConfiguration

---
+++ Session-30/GZFuel.EF/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using GZFuel.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.EF.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+           
+            //table 
+            builder.ToTable("Customer");
+            builder.HasKey(c => c.ID);
+            
+            //properties
+            builder.Property(c => c.Name).HasMaxLength(50).IsRequired();
+            builder.Property(c => c.Surname).HasMaxLength(50).IsRequired();
+            builder.Property(c => c.CardNumber).HasMaxLength(50).IsRequired();
+           
+            //relationships
+            
+
+
+
+        }
+    }
+}
