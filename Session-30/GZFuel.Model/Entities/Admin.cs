7c9691bb092a68f68b98ad21d2477dad15ade5ef
Georgios Zacharidis <zageo@outlook.com>
2023-02-18 11:42:13 +0200

    Added Admin Class

---
+++ Session-30/GZFuel.Model/Entities/Admin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.Model.Entities
+{
+
+    // this entity is just for experimental reasons , maybe a way to implement the admin login on the windows forms app. 
+    public class Admin
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }  
+        public int Surname { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+        public Admin(int id, int name, int surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+
+        public Admin() { }
+        
+    }
+}
