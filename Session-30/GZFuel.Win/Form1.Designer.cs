69bffa60ef47a629737fabf62b0d33a1735388cd
Georgios Zacharidis <zageo@outlook.com>
2023-02-18 23:39:42 +0200

    minor ui fix

--- Session-30/GZFuel.Win/Form1.Designer.cs
+++ Session-30/GZFuel.Win/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 34);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Manager Area";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
