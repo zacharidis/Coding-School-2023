80ae52b0e1494c6150125e5ae4f3f9e08f97d15b
Georgios Zacharidis <zageo@outlook.com>
2023-02-18 11:12:53 +0200

    DbContext reference

--- Session-30/GZFuel.EF/FuelDbContext.cs
+++ Session-30/GZFuel.EF/FuelDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace GZFuel.EF
 {
-    public class FuelDbContext
+    // add the dbcontext here :)
+    public class FuelDbContext : DbContext 
     {
+
     }
 }
