8b1d65ea2f8df7a305cc9d4070a19b6be2cf812b
Georgios Zacharidis <zageo@outlook.com>
2023-02-18 10:56:35 +0200

    Finalized Employee class

--- Session-30/GZFuel.Model/Entities/Employee.cs
+++ Session-30/GZFuel.Model/Entities/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GZFuel.Model.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace GZFuel.Model.Entities
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime HireDateStart { get; set; }
+        public DateTime HireDateEnd { get;set; }
+
+        public int SalaryPerMonth { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+
+       // Constructor 
+       
+        public Employee(string name , string surname , int salaryPerMonth , EmployeeType employeeType)
+        {
+
+            Name = name;
+            Surname = surname;
+            SalaryPerMonth = salaryPerMonth;
+            EmployeeType = employeeType;
+            Transactions = new List<Transaction>();
+        }
+
+        //relationships 
+
+        public List<Transaction> Transactions { get; set; }
+
     }
 }
