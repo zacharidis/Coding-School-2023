0e69258beac992e7385623743128fc3da958d895
Georgios Zacharidis <zageo@outlook.com>
2023-02-19 10:33:42 +0200

    now a public class

--- Session-30/GZFuel.EF/Configurations/EmployeeConfiguration.cs
+++ Session-30/GZFuel.EF/Configurations/EmployeeConfiguration.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace GZFuel.EF.Configurations
 {
-    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
     {
         public void Configure(EntityTypeBuilder<Employee> builder)
         {
