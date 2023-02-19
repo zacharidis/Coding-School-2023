8f58cbffdfd7163c85113180c3dba94a036b7872
Georgios Zacharidis <zageo@outlook.com>
2023-02-19 21:10:02 +0200

    Added Base Entity

---
+++ Session-30/GZFuel.Model/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.Model.Entities
+{
+
+    // all models will inherit from this class 
+    // it helps with the creation of the database while using the EF 
+
+    public class BaseEntity
+    {
+        public BaseEntity() { }
+    }
+}
