63ecbce001edbd86bf8dcc8ad500067cbc053745
Georgios Zacharidis <zageo@outlook.com>
2023-02-19 19:33:00 +0200

    First Push

--- Session-30/GZFuel.EF/Configurations/TransactionLineConfiguration.cs
+++ Session-30/GZFuel.EF/Configurations/TransactionLineConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GZFuel.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace GZFuel.EF.Configurations
 {
-    internal class TransactionLineConfiguration
+    public class TransactionLineConfiguration : IEntityTypeConfiguration<TransactionLine>
     {
+        public void Configure(EntityTypeBuilder<TransactionLine> builder)
+        {
+            builder.ToTable("TransactionLine");
+        }
     }
 }
