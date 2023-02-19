dea4f1dc4cdc7ab8639327bb8f02f2746d1718a7
Georgios Zacharidis <zageo@outlook.com>
2023-02-19 10:22:45 +0200

    Minor Fix in Employee Configuration

--- Session-30/GZFuel.EF/Configurations/EmployeeConfiguration.cs
+++ Session-30/GZFuel.EF/Configurations/EmployeeConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GZFuel.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,43 @@ using System.Threading.Tasks;
 
 namespace GZFuel.EF.Configurations
 {
-    internal class EmployeeConfiguration
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
     {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            
+            //table 
+            builder.ToTable("Employee");
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).ValueGeneratedOnAdd().IsRequired(true);
+
+            //properties
+
+            builder.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsRequired(true);
+            builder.Property(e => e.Surname)
+                .HasMaxLength(50)
+                .IsRequired(true);
+            builder.Property(e => e.HireDateStart)
+                .IsRequired(true);
+            builder.Property(e => e.HireDateEnd)
+                .IsRequired(false); // employee can be fired so it's nullable 
+
+            builder.Property(e => e.SalaryPerMonth).HasColumnType("integer").IsRequired(true);
+            builder.Property(e => e.EmployeeType).IsRequired(true);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }
