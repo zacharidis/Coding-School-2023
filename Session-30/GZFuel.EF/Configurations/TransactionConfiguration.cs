ce204f1c0f9ddac8b53eb55ef5feb55a53bef1c1
Georgios Zacharidis <zageo@outlook.com>
2023-02-19 17:43:34 +0200

    TransactionConfiguration

--- Session-30/GZFuel.EF/Configurations/TransactionConfiguration.cs
+++ Session-30/GZFuel.EF/Configurations/TransactionConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GZFuel.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,16 @@ using System.Threading.Tasks;
 
 namespace GZFuel.EF.Configurations
 {
-    internal class TransactionConfiguration
+    internal class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
     {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transaction");
+            builder.HasKey(t => t.ID);
+            builder.Property(t => t.ID).ValueGeneratedOnAdd();
+
+            //properties 
+
+        }
     }
 }
