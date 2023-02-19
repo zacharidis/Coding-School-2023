937dae02ab6a3d6cdbd975d4d73b3866c99fe2c1
Georgios Zacharidis <zageo@outlook.com>
2023-02-19 11:05:31 +0200

    Fixed mistake in naming String instead of Enum

--- Session-30/GZFuel.Model/Entities/Item.cs
+++ Session-30/GZFuel.Model/Entities/Item.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GZFuel.Model.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace GZFuel.Model.Entities
          public int ID { get; set; }
         public string Code { get; set; }
         public string Description { get; set; }
-        public string ItemType { get; set; }
+        public ItemType ItemType { get; set; }
         public decimal Price { get; set; }
         public decimal Cost { get; set; }
 
