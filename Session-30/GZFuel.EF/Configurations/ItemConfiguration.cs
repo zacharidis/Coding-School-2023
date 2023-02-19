1f91315d94b3ef466ce6d4ed5c890b60f543fd56
Georgios Zacharidis <zageo@outlook.com>
2023-02-19 10:44:20 +0200

    WIP

--- Session-30/GZFuel.EF/Configurations/ItemConfiguration.cs
+++ Session-30/GZFuel.EF/Configurations/ItemConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GZFuel.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace GZFuel.EF.Configurations
 {
-    public class ItemConfiguration
+    public class ItemConfiguration : IEntityTypeConfiguration<Item>
     {
+      
+
+        public void Configure(EntityTypeBuilder<Item> builder)
+        {
+            //table
+            builder.ToTable("Item");
+            builder.HasKey(i => i.ID);
+            builder.Property(i => i.ID).ValueGeneratedOnAdd();
+
+            //properties
+            builder.Property(i => i.Code)
+                .HasMaxLength(10)
+                .IsRequired(true);  
+
+            builder.Property(i=> i.Cost).HasPrecision(3,2).IsRequired(true);
+            builder.Property(i=> i.Price).HasPrecision(3,2).IsRequired(true);   
+            builder.Property(i => i.Description).HasMaxLength(250).IsRequired(true);
+            builder.Property(i => i.ItemType).HasMaxLength(50).IsRequired(true);
+                
+
+
+
+
+
+
+
+
+
+        }
     }
 }
