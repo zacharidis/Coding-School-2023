ce19e48937a3c96c68ed532ac372b444fa74309b
Georgios Zacharidis <zageo@outlook.com>
2023-02-20 11:12:54 +0200

    Added AdminRepositry

---
+++ Session-30/GZFuel.EF/Repositories/AdminRepository.cs
@@ -0,0 +1,50 @@
+﻿using GZFuel.EF.Context;
+using GZFuel.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.EF.Repositories
+{
+    public class AdminRepository : IEntityRepo<Admin>
+    {
+        // Admin can be only one so we can only update the password here 
+
+        public void Update(int id, Admin entity)
+        {
+            
+        }
+
+        public IEnumerable<Admin> GetAll()
+        {
+            using var ctx = new FuelDbContext();
+            return ctx.Admins.Include(a => a.Password).ToList();
+            
+        }
+
+
+
+        // - unused methods
+        public void Add(Admin entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        public Admin? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+    }
+}
