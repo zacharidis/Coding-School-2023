f52dd06e6302bbd8eb3d7858ebf28e4a6df90b2c
Georgios Zacharidis <zageo@outlook.com>
2023-02-20 13:31:05 +0200

    Add Item method finished

---
+++ Session-30/GZFuel.EF/Repositories/ItemRepository.cs
@@ -0,0 +1,49 @@
+﻿using GZFuel.EF.Context;
+using GZFuel.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.EF.Repositories
+{
+    public class ItemRepository : IEntityRepo<Item>
+    {
+        public void Add(Item entity)
+        {
+            using var ctx = new FuelDbContext();
+            if (entity.ID == 0) {
+                
+                throw new ArgumentException("Item cannot have a predefined ID", nameof(entity));
+            } else
+            {
+                ctx.Items.Add(entity);
+                ctx.SaveChanges();
+            }
+                
+            
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Item entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
