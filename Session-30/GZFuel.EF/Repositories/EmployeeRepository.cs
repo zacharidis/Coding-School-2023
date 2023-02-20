5c968422441ec2d8f87544c2e763b2f199e46693
Georgios Zacharidis <zageo@outlook.com>
2023-02-20 13:20:20 +0200

    Finished EmployeeRepository

--- Session-30/GZFuel.EF/Repositories/EmployeeRepository.cs
+++ Session-30/GZFuel.EF/Repositories/EmployeeRepository.cs
@@ -57,12 +57,58 @@ namespace GZFuel.EF.Repositories
 
         public Employee? GetById(int id)
         {
-            throw new NotImplementedException();
+            using var ctx = new FuelDbContext();
+            var dbEmployee = ctx.Employees.Where(e => e.Id == id).SingleOrDefault();
+            
+            if (dbEmployee != null)
+            {
+
+                return dbEmployee;
+            } else {
+
+                throw new KeyNotFoundException($"Employee with id '{id}' could not be found");
+            
+            }
+
+            
+
         }
 
         public void Update(int id, Employee entity)
         {
-            throw new NotImplementedException();
+            using var  ctx  = new FuelDbContext();
+            var selectedEmployee = ctx.Employees.SingleOrDefault(e=> e.Id == id);
+            if (selectedEmployee != null)
+            {
+                selectedEmployee.Name= entity.Name;
+                selectedEmployee.Surname= entity.Surname;
+                selectedEmployee.SalaryPerMonth= entity.SalaryPerMonth;
+                selectedEmployee.EmployeeType= entity.EmployeeType;
+                // selectedEmployee.HireDateEnd= entity.HireDateEnd;
+                // i have to implement the employee to get fired , maybe a boolean property ?
+                ctx.SaveChanges();
+            } else
+            {
+                throw new KeyNotFoundException($"'{id}' Employee coud not be found");
+            }
+
         }
+
+        public void getFired(int id)
+        {
+            using var ctx = new FuelDbContext();
+            var selectedEmployee = ctx.Employees.Include(e => e.Id).SingleOrDefault();
+            if (selectedEmployee != null)
+            {
+                selectedEmployee.HireDateEnd = DateTime.Now;
+                ctx.SaveChanges();
+
+            } else {
+                
+                throw new KeyNotFoundException($"{id} could not be found");
+            
+            }
+        }
+
     }
 }
