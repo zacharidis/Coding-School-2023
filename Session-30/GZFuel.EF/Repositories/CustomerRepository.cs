425d47547ab1aa286d636d7c4b152779bb06e1dd
Georgios Zacharidis <zageo@outlook.com>
2023-02-20 10:25:08 +0200

    Delete in Customer Repo

---
+++ Session-30/GZFuel.EF/Repositories/CustomerRepository.cs
@@ -0,0 +1,56 @@
+﻿using GZFuel.EF.Context;
+using GZFuel.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.EF.Repositories
+{
+    public class CustomerRepository : IEntityRepo<Customer>
+    {
+        public void Add(Customer entity)
+        {
+            using var ctx = new FuelDbContext();
+            if (entity == null)
+            {
+                throw new Exception("Entity is not complete");
+            } else
+            {
+                ctx.Add(entity);
+                ctx.SaveChanges();
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            using var ctx = new FuelDbContext();
+            //go get the specific Customer
+            var dbCustomer = ctx.Customers
+                .Where(e => e.ID== id)
+                .SingleOrDefault();
+            ctx.Remove(dbCustomer);
+            ctx.SaveChanges();
+            // maybe i could use the GetById to find the c ustomer for less code. 
+
+        }
+
+        public IList<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
