7b15e58f55aff19df21eb0bbb5b1f5b0e4e8e78d
Georgios Zacharidis <zageo@outlook.com>
2023-02-20 11:23:03 +0200

    Added Add Employee Method

--- Session-30/GZFuel.EF/Repositories/EmployeeRepository.cs
+++ Session-30/GZFuel.EF/Repositories/EmployeeRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GZFuel.EF.Context;
+using GZFuel.Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,40 @@ using System.Threading.Tasks;
 
 namespace GZFuel.EF.Repositories
 {
-    internal class EmployeeRepository
+    public class EmployeeRepository : IEntityRepo<Employee>
     {
+        public void Add(Employee entity)
+        {
+            using var ctx = new FuelDbContext();
+            if (entity.Id != 0)
+            {
+                throw new ArgumentException("Employee cannot have a predefined ID");
+            }
+            else
+            {
+                ctx.Employees.Add(entity);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Employee entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
