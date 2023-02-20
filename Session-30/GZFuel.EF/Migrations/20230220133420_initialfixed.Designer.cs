6b2b3f168a08b2dec48fa7d82da9039e7cceed07
Georgios Zacharidis <zageo@outlook.com>
2023-02-20 15:34:43 +0200

    mig

--- Session-30/GZFuel.EF/Migrations/20230220133420_initialfixed.Designer.cs
+++ Session-30/GZFuel.EF/Migrations/20230220133420_initialfixed.Designer.cs
@@ -4,6 +4,7 @@ using GZFuel.EF.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GZFuel.EF.Migrations
 {
     [DbContext(typeof(FuelDbContext))]
-    partial class FuelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230220133420_initialfixed")]
+    partial class initialfixed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
