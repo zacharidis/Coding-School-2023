1c36a28636621d216dbdf4119898c1f81a9a1240
Georgios Zacharidis <zageo@outlook.com>
2023-02-21 08:54:08 +0200

    Added AdminController

---
+++ Session-30/GZFuel.Blazor/Server/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GZFuel.Blazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from AdminController");
+        }
+    }
+}
