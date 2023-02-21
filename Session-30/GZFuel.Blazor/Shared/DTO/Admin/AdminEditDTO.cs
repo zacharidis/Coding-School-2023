2c91a6ef0f2d5c2a8a784e5fae160045d99627ca
Georgios Zacharidis <zageo@outlook.com>
2023-02-21 16:31:34 +0200

    Added new DTO for admin

---
+++ Session-30/GZFuel.Blazor/Shared/DTO/Admin/AdminEditDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.Blazor.Shared.DTO.Admin
+{
+    public class AdminEditDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
