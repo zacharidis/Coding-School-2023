b2f72f775b7d23c34a82779a3e49d2872e4e0256
Georgios Zacharidis <zageo@outlook.com>
2023-02-22 23:51:46 +0200

    it's alive !

--- Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/Form1.Designer.cs
+++ Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/Form1.Designer.cs
@@ -28,12 +28,46 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			button1 = new Button();
+			dataGridView1 = new DataGridView();
+			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(12, 26);
+			button1.Name = "button1";
+			button1.Size = new Size(75, 23);
+			button1.TabIndex = 0;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// dataGridView1
+			// 
+			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridView1.Location = new Point(21, 66);
+			dataGridView1.Name = "dataGridView1";
+			dataGridView1.RowTemplate.Height = 25;
+			dataGridView1.Size = new Size(636, 358);
+			dataGridView1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(dataGridView1);
+			Controls.Add(button1);
+			Name = "Form1";
+			Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+			ResumeLayout(false);
 		}
 
 		#endregion
+
+		private Button button1;
+		private DataGridView dataGridView1;
 	}
 }
--- Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/Form1.cs
+++ Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/Form1.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace GZFuelWinClient
 {
 	public partial class Form1 : Form
@@ -5,6 +11,15 @@ namespace GZFuelWinClient
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			HttpClient client = new HttpClient();
+			client.BaseAddress = new Uri("https://localhost:7068/");
+			var response = client.GetAsync("customer").Result;
+			var emp = response.Content.ReadFromJsonAsync<IEnumerable<Customer>>().Result;
+			dataGridView1.DataSource = emp;
 		}
 	}
 }
