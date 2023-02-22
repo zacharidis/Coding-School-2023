d6ac82dc3990595e8f7306648ec29e79cc4a88f5
Georgios Zacharidis <zageo@outlook.com>
2023-02-22 13:56:13 +0200

    Added Customer Controller

--- Session-30/GZFuel.Blazor/Server/Controllers/CustomerController.cs
+++ Session-30/GZFuel.Blazor/Server/Controllers/CustomerController.cs
@@ -10,7 +10,13 @@ namespace GZFuel.Blazor.Server.Controllers
 	[ApiController]
 	public class CustomerController : ControllerBase
 	{
-
+		// all controllers should have a repo 
+		// and a constructor to inject the repo
+		// so we have a Get method to get all the customers
+		// and a Get method to get a customer by id
+		//TODO  and we have a Post method to create a new customer
+		//TODO  and we have a Put method to update a customer
+		//TODO  and we have a Delete method to delete a customer
 
 		private readonly IEntityRepo<Customer> _customerRepo;
 
@@ -58,11 +64,36 @@ namespace GZFuel.Blazor.Server.Controllers
 					CardNumber = result.CardNumber
 				};
 			}
+
+
+
+		// Create a new Customer
+
+
+		[HttpPost]
+		public async Task Post(CustomerEditDTO customer)
+		{
+			var newCustomer  = new Customer(customer.Name,customer.Surname,customer.CardNumber)
+			{
+				ID = customer.Id,
+				Name = customer.Name,
+				Surname = customer.Surname,
+				CardNumber = customer.CardNumber
+			};
+
+			_customerRepo.Add(newCustomer);
+			
+		}
+
+
+
+
 		
 
 		}
 
-	
+	  
+	  
 
 
 
