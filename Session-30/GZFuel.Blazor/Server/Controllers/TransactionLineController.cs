b2df3f6c5783a5a6f0cc9ecaa26342efcb249c07
Georgios Zacharidis <zageo@outlook.com>
2023-02-22 16:37:30 +0200

    Added TSL controller

---
+++ Session-30/GZFuel.Blazor/Server/Controllers/TransactionLineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GZFuel.Blazor.Server.Controllers
+{
+	public class TransactionLineController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
