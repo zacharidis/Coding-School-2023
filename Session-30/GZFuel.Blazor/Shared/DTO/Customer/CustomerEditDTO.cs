5036dcb864366afcc0437fdaaf1397e17aeea605
Georgios Zacharidis <zageo@outlook.com>
2023-02-22 11:38:20 +0200

    Added DTO for editing the customer

---
+++ Session-30/GZFuel.Blazor/Shared/DTO/Customer/CustomerEditDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.Blazor.Shared.DTO.Customer
+{
+	public class CustomerEditDTO
+	{
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string CardNumber { get; set; }
+
+
+	}
+}
