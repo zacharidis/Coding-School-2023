b7de8becf5f58ba9931954aaba757639ae5e6d01
Georgios Zacharidis <zageo@outlook.com>
2023-02-22 16:37:22 +0200

    Added Transaction Controller

---
+++ Session-30/GZFuel.Blazor/Server/Controllers/TransactionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GZFuel.Blazor.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TransactionController : ControllerBase
+	{
+	}
+}
