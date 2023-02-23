f471dbfad2324071a38f28b98542f9290ec95a0f
Georgios Zacharidis <zageo@outlook.com>
2023-02-23 11:34:51 +0200

    EmpEditDTO

---
+++ Session-30/GZFuel.Blazor/Shared/DTO/Employee/EmployeeEditDTO.cs
@@ -0,0 +1,23 @@
+﻿using GZFuel.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.Blazor.Shared.DTO.Employee
+{
+    public class EmployeeEditDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public DateTime HireDateStart { get; set; }
+        public DateTime? HireDateEnd { get; set; }
+        public int SalaryPerMonth { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+
+        public EmployeeEditDTO() { }
+    }
+}
