b26c2a8cd0c1957bc06a4fa2a1cc0b3462d86891
Georgios Zacharidis <zageo@outlook.com>
2023-02-23 16:29:40 +0200

    Fixed DTO for Emp

--- Session-30/GZFuel.Blazor/Shared/DTO/Employee/EmployeeEditDTO.cs
+++ Session-30/GZFuel.Blazor/Shared/DTO/Employee/EmployeeEditDTO.cs
@@ -1,6 +1,7 @@
 ﻿using GZFuel.Model.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,18 @@ namespace GZFuel.Blazor.Shared.DTO.Employee
     public class EmployeeEditDTO
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Surname { get; set; }
-
+        [Required]
         public DateTime HireDateStart { get; set; }
         public DateTime? HireDateEnd { get; set; }
-        public int SalaryPerMonth { get; set; }
-        public EmployeeType EmployeeType { get; set; }
+        
+        [Required]
+		public int SalaryPerMonth { get; set; }
+        [Required]
+		public EmployeeType EmployeeType { get; set; }
 
         public EmployeeEditDTO() { }
     }
