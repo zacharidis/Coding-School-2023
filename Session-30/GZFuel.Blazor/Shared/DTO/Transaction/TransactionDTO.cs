b4697f40416f3ed7da087a2f837ebdc763b50fde
Georgios Zacharidis <zageo@outlook.com>
2023-02-23 10:43:24 +0200

    Added TransactionDTO

---
+++ Session-30/GZFuel.Blazor/Shared/DTO/Transaction/TransactionDTO.cs
@@ -0,0 +1,23 @@
+﻿using GZFuel.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.Blazor.Shared.DTO.Transaction
+{
+    public  class TransactionDTO
+    {
+
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+
+        public PaymentMethod PaymentMethod { get; set; }
+        public decimal TotalValue { get; set; }
+
+
+
+        public TransactionDTO() { }
+    }
+}
