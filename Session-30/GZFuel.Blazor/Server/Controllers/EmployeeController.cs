36edcb7d3e1d0716fdd341a191f365c6be130621
Georgios Zacharidis <zageo@outlook.com>
2023-02-23 11:23:04 +0200

    Added Eployee Controller

---
+++ Session-30/GZFuel.Blazor/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using GZFuel.EF.Repositories;
+using GZFuel.Model.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GZFuel.Blazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly IEntityRepo<Employee> _employeeRepo;
+        public EmployeeController(IEntityRepo<Employee> employeeRepo)
+        {
+            _employeeRepo = employeeRepo;
+        }
+
+
+
+
+
+
+
+    }
+}
