a881fc8d1d6a3a4354bdfdec2a999c1f8eb0c292
Georgios Zacharidis <zageo@outlook.com>
2023-02-24 16:17:00 +0200

    Added transaction class

---
+++ Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using GZFuelWinClient.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuelWinClient
+{
+	public class Transaction
+	{
+		public int ID { get; set; }
+		public DateTime Date { get; set; }
+
+		public int EmployeeID { get; set; }
+		public int CustomerID { get; set; }
+
+		public PaymentMethod PaymentMethod { get; set; }
+		public decimal TotalValue { get; set; }
+
+	}
+}
