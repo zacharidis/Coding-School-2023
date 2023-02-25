718433a0f8abe1f09388d25f13c47504a4d82a18
Georgios Zacharidis <zageo@outlook.com>
2023-02-25 16:27:57 +0200

    Added ItemDTO for returning all Items from the repo

---
+++ Session-30/GZFuel.Blazor/Shared/DTO/Item/ItemDTO.cs
@@ -0,0 +1,19 @@
+﻿using GZFuel.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.Blazor.Shared.DTO.Item
+{
+    public class ItemDTO
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public ItemType ItemType { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
