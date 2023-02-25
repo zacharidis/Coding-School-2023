4abcb80c707b0414032fff099d1706703db8969e
Georgios Zacharidis <zageo@outlook.com>
2023-02-25 14:24:29 +0200

    added form for item

--- Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/frmItems.Designer.cs
+++ Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/frmItems.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmItems";
+            SuspendLayout();
+            // 
+            // frmItems
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(695, 510);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "frmItems";
+            Text = "frmItems";
+            ResumeLayout(false);
         }
 
         #endregion
