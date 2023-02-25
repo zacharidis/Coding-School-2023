f9c54d3beeb44f4fbe8e217a418cef18e8c55da2
Georgios Zacharidis <zageo@outlook.com>
2023-02-25 12:56:22 +0200

    revamping the form

--- Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/frmTransactions.Designer.cs
+++ Session-30/WindowsClientGZF/GZFuelWinClient/GZFuelWinClient/frmTransactions.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(846, 540);
+            ClientSize = new Size(942, 540);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "frmTransactions";
             Text = "Transactions";
