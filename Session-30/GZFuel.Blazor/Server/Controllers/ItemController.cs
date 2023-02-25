21a1b7d6efc53354256a7f91f20edef712275389
Georgios Zacharidis <zageo@outlook.com>
2023-02-25 16:25:33 +0200

    Added private repo inside the cotnroller

--- Session-30/GZFuel.Blazor/Server/Controllers/ItemController.cs
+++ Session-30/GZFuel.Blazor/Server/Controllers/ItemController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GZFuel.EF.Repositories;
+using GZFuel.Model.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GZFuel.Blazor.Server.Controllers
@@ -7,5 +9,12 @@ namespace GZFuel.Blazor.Server.Controllers
 	[ApiController]
 	public class ItemController : ControllerBase
 	{
+		private readonly IEntityRepo<Item> _itemRepo;
+
+		public ItemController(IEntityRepo<Item> itemRepo)
+		{
+            _itemRepo = itemRepo;
+        }
+
 	}
 }
