3d1817e6286f3885ab5b1334b8bfd321a138eccc
Georgios Zacharidis <zageo@outlook.com>
2023-02-27 12:40:14 +0200

    Deleted testing page for admin

---
+++ Session-30/GZFuel.Blazor/Shared/DTO/Admin/AdminEditValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZFuel.Blazor.Shared.DTO.Admin
+{
+	public class AdminEditValidator : AbstractValidator<AdminEditDTO>
+	{
+		public AdminEditValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Please enter a Name");
+			RuleFor(x => x.Surname).NotEmpty().WithMessage("Please enter a Surname");
+			RuleFor(x => x.Username).NotEmpty().WithMessage("Please enter a Username");
+			RuleFor(x => x.Password).NotEmpty().WithMessage("Please enter a Password");
+		}
+	}
+}
