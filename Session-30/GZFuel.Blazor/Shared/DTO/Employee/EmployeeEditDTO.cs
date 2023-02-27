036dde820015ad52fc22a98722802766353a38a6
Georgios Zacharidis <zageo@outlook.com>
2023-02-27 10:27:25 +0200

    Added Validations , But the forms have to be redesigned

--- Session-30/GZFuel.Blazor/Shared/DTO/Employee/EmployeeEditDTO.cs
+++ Session-30/GZFuel.Blazor/Shared/DTO/Employee/EmployeeEditDTO.cs
@@ -11,17 +11,17 @@ namespace GZFuel.Blazor.Shared.DTO.Employee
     public class EmployeeEditDTO
     {
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [Required]
-        public string Surname { get; set; }
+		[Required(ErrorMessage = "Please enter a Name")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Please enter a Surname")]
+		public string Surname { get; set; }
         [Required]
         public DateTime HireDateStart { get; set; }
         public DateTime? HireDateEnd { get; set; }
-        
-        [Required]
+
+		[Required(ErrorMessage = "Please enter a Salary")]
 		public int SalaryPerMonth { get; set; }
-        [Required]
+		[Required(ErrorMessage = "Please Choose the Employee Type ")]
 		public EmployeeType EmployeeType { get; set; }
 
         public EmployeeEditDTO() { }
