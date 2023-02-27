3df011110a19ab14efc593c978fa6d75357602b4
Georgios Zacharidis <zageo@outlook.com>
2023-02-27 12:20:23 +0200

    added validations messages for testing

--- Session-30/GZFuel.Blazor/Shared/DTO/Admin/AdminEditDTO.cs
+++ Session-30/GZFuel.Blazor/Shared/DTO/Admin/AdminEditDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,16 @@ namespace GZFuel.Blazor.Shared.DTO.Admin
     public class AdminEditDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-
-        public string Surname { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
+       
+		[Required(ErrorMessage = "Please enter a Name")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Please enter a Surname")]
+		public string Surname { get; set; }
+		
+		[Required(ErrorMessage = "Please enter a Username")]
+		public string Username { get; set; }
+		[Required(ErrorMessage = "Please enter a Password")]
+		public string Password { get; set; }
+		
+	}
 }
